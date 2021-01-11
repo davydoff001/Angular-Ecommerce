f2aa82b02f8ead3b9dbe59d1d2be6005a4776b78
a.davydov <mrdavudoff@ukr.net>
2021-01-11 12:39:46 +0200

    Login + registration

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+    oidc: {
+        clientId: '0oa3nhqgsMjvn2xKW5d6',
+        issuer: 'https://dev-3742022.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+
+    }
+}
